9dbcc121de8cd4d7e4a464aaabaa001499fbfdc2
SaiTp <37739724+SaiTp@users.noreply.github.com>
2021-03-16 22:39:11 +0530

    Time: 4 ms (62.86%), Space: 6.8 MB (30.02%) - LeetHub

---
+++ shortest-distance-to-a-character/shortest-distance-to-a-character.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> shortestToChar(string s, char c) {
+         vector<int> ioc; 
+        int n=s.length();
+        vector<int>ans; 
+        for(int i=0;i<n;++i){
+            if(s[i]==c) 
+                ioc.push_back(i);
+        }
+        int m=ioc.size(); 
+        int left=0,right=0;
+        for(int i=0;i<n;++i)
+        {
+            if(i>ioc[right])
+            {
+                left=right;
+                if(right<m-1)
+                    ++right;
+            }
+           ans.push_back(min(abs(ioc[right]-i),abs(ioc[left]-i))); 
+        }    
+        return ans; 
+    }
+};

13866e2d6634a7975373afd0e25d1bf83dfaf4a2
SaiTp <37739724+SaiTp@users.noreply.github.com>
2021-03-18 21:40:32 +0530

    Time: 406 ms (25.22%), Space: 0B (100.00%) - LeetHub

---
+++ employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select e1.Name as Employee from Employee e1, Employee e2 
+where e1.ManagerId is not null 
+and 
+e1.ManagerId=e2.Id
+and 
+e1.Salary > e2.Salary;

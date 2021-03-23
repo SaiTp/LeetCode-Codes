7390bdb328fa44ee48e72029779946fa2e023c73
SaiTp <37739724+SaiTp@users.noreply.github.com>
2021-03-23 16:15:29 +0530

    Time: 4 ms (99.41%), Space: 12.7 MB (85.74%) - LeetHub

---
+++ jump-game/jump-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+     int index = 0;
+        for(int i = nums.size()-1;i>=0;i--)
+        {
+            if(i + nums[i] >= index)
+                index = i;
+        }
+        return index == 0;   
+    }
+};

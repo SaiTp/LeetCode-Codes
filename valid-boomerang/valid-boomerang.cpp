79f824bdff8f5d11fee8695ee4b3919ea90de1e3
SaiTp <37739724+SaiTp@users.noreply.github.com>
2021-03-26 16:28:44 +0530

    Time: 0 ms (100.00%), Space: 10.4 MB (8.33%) - LeetHub

---
+++ valid-boomerang/valid-boomerang.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isBoomerang(vector<vector<int>>& points) {
+         
+        int x1=points[1][0]-points[0][0];
+        int x2=points[2][0]-points[0][0];
+        int y1=points[1][1]-points[0][1];
+        int y2=points[2][1]-points[0][1];
+        
+        return x1*y2!=x2*y1;
+    }
+};

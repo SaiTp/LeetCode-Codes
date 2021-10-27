675e8fbb9c7248ed57f7ced8eb05b06f646f54c5
SAiTEj <37739724+SaiTp@users.noreply.github.com>
2021-10-27 19:51:52 +0530

    Time: 8 ms (76.54%), Space: 14 MB (92.76%) - LeetHub

---
+++ final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int res=0;
+        for(int i=0;i<operations.size();i++)
+            if(operations[i][0]=='+' || operations[i][2]=='+')
+                res++;
+            else
+                res--;
+        return res;
+    }
+};

3104a8e36ad99012039194b6e7facda5c82a55ea
SAiTEj <37739724+SaiTp@users.noreply.github.com>
2022-01-06 12:55:45 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (55.86%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
@@ -1,14 +1,9 @@
 class Solution {
 public:
     bool checkString(string s) {
-        int f=1;
-        for(int i=0;i<s.size();i++)
-        {
-            if(s[i]=='b')
-                f=0;
-            if(!f && s[i]=='a')
+        for(int i=0;i<s.length()-1;i++)
+            if(s[i]>s[i+1])
                 return false;
-        }
         return true;
     }
 };

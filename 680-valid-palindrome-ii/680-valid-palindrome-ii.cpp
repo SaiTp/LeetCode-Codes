f9bb20bab5c2c9f8369e7a85e997aae21fc956a1
SAiTEj <37739724+SaiTp@users.noreply.github.com>
2022-04-02 10:17:35 +0530

    Time: 78 ms (34.22%), Space: 24 MB (31.50%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+         int l = 0, r = s.size() - 1;
+        for (int l = 0, r = s.size() - 1; l < r; l ++, r --) {
+            if (s[l] != s[r]) {
+                if (check(l + 1, r, s)) return true;
+                if (check(l, r - 1, s)) return true;
+                return false;
+            }
+        }
+        return true;
+    }
+    bool check(int l, int r, string s) {
+        for (; l < r; l ++, r --) {
+            if (s[l] != s[r]) return false;
+        }
+        return true;
+    }
+};

1f05d63b8f746599a448bcc5c1b2d092cfbe8975
SAiTEj <37739724+SaiTp@users.noreply.github.com>
2022-04-26 16:17:11 +0530

    Time: 186 ms (86.59%), Space: 42.2 MB (79.88%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        
+        // Min-heap to store minimum weight edge at top.
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> heap;
+        
+        // Track nodes which are included in MST.
+        vector<bool> inMST(n);
+        
+        heap.push({ 0, 0 });
+        int mstCost = 0;
+        int edgesUsed = 0;
+        
+        while (edgesUsed < n) {
+            pair<int, int> topElement = heap.top();
+            heap.pop();
+            
+            int weight = topElement.first;
+            int currNode = topElement.second;
+            
+            // If node was already included in MST we will discard this edge.
+            if (inMST[currNode]) {
+                continue;
+            }
+            
+            inMST[currNode] = true;
+            mstCost += weight;
+            edgesUsed++;
+            
+            for (int nextNode = 0; nextNode < n; ++nextNode) {
+                // If next node is not in MST, then edge from curr node
+                // to next node can be pushed in the priority queue.
+                if (!inMST[nextNode]) {
+                    int nextWeight = abs(points[currNode][0] - points[nextNode][0]) + 
+                                     abs(points[currNode][1] - points[nextNode][1]);
+                    
+                    heap.push({ nextWeight, nextNode });
+                }
+            }
+        }
+        
+        return mstCost;
+    }
+};

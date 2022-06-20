859c40414dee085f1a091596632389ca626a1fb0
SAiTEj <37739724+SaiTp@users.noreply.github.com>
2022-06-20 10:00:31 +0530

    Time: 33 ms (98.51%), Space: 13.7 MB (96.04%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
-   static bool compare (string& first, string& second) {
-        return first.size() > second.size();
-    }
     int minimumLengthEncoding(vector<string>& words) {
-        unordered_map<string, int> mp;
-        int ans = 0;
-        
-        //sorting the array in decreasing length sequence
-        sort(words.begin(), words.end(), compare);
-
-        //Entering each word in hash map.
-        for(int i=0; i<words.size(); i++){
-            mp[words[i]]++;
+        int n=words.size();
+        if(n==1)
+            return words[0].size()+1;
+       for(auto& w:words) 
+           reverse(w.begin(),w.end());
+        sort(words.begin(), words.end(), greater<string>());
+        int cnt=0;
+        for(int i=0; i<words.size(); i++) {
+            if(i && words[i-1].find(words[i]) == 0) continue;
+            cnt+=words[i].size()+1;
         }
-        
-        //For each word if it exist in hash map then add its length+1 to ans as # is also included.
-        for(int i=0; i<words.size(); i++){
-            int k = words[i].size();
-            if(mp[words[i]] >0){
-                ans += (k+1);
-            }
-            
-            //Deleting record of any suffix of the word from hash map as it is already covered.
-            for(int j=k-1; j>=0; j--){
-                string s = words[i].substr(j, k-j);
-                mp[s] = 0;
-            }
-        }
-        return ans;
+        return cnt;
     }
 };

e0e3ff21e4748c1d8d09adfedf423591557bca2c
SAiTEj <37739724+SaiTp@users.noreply.github.com>
2022-06-28 16:16:09 +0530

    Time: 18 ms (45.52%), Space: 12.3 MB (89.13%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     vector<string> removeAnagrams(vector<string>& words) {
-        for(int i=0;i<words.size()-1;i++)
+       std::vector<std::string> dest;
+        std::string lastKey;
+        for (std::string const &word : words)
         {
-            string t=words[i];
-            sort(t.begin(),t.end());
-            for(int j=i+1;j<words.size();j++)
+            std::string key(word);
+            std::sort(key.begin(), key.end());
+            if (key != lastKey)
             {
-                string f=words[j];
-                sort(f.begin(),f.end());
-                if(f==t)
-                {
-                    words.erase(words.begin()+j);
-                    j--;
-                }
-                else
-                    break;
+                dest.push_back(word);
+                std::swap(lastKey, key);
             }
         }
-        return words;
+        return dest;
     }
 };

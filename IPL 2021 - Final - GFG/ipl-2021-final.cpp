9d118deee2b67f1d664245918a031e9e0c087fc7
SAiTEj <37739724+SaiTp@users.noreply.github.com>
2022-07-04 11:07:26 +0530

    Added solution - LeetHub

---
+++ IPL 2021 - Final - GFG/ipl-2021-final.cpp
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+// User function template for c++
+class Solution {
+  public:
+    int findMaxLen(string s) {
+        // code here
+        stack<int>st;
+        st.push(-1);
+        int ans=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='(')
+            st.push(i);
+            else
+            {
+                st.pop();
+                if(st.empty())
+                st.push(i);
+                else
+                ans=max(ans,i-st.top());
+            }
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S; 
+        Solution ob;
+        cout << ob.findMaxLen(S) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends

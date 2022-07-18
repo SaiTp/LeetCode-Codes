58128604d7c83fb394c80e9965bb923a8281f7ef
SAiTEj <37739724+SaiTp@users.noreply.github.com>
2022-07-18 12:01:01 +0530

    Time: 4 ms (64.89%), Space: 7.7 MB (93.56%) - LeetHub

---
+++ 677-map-sum-pairs/677-map-sum-pairs.cpp
@@ -0,0 +1,28 @@
+class MapSum {
+public:
+    unordered_map<string,int>mp;
+    MapSum() {
+        
+    }
+    
+    void insert(string key, int val) {
+        mp[key]=val;
+    }
+    
+    int sum(string prefix) {
+        int res=0;
+        for(auto i:mp)
+        {
+            if(i.first.find(prefix)==0)
+                res+=i.second;
+        }
+        return res;
+    }
+};
+
+/**
+ * Your MapSum object will be instantiated and called as such:
+ * MapSum* obj = new MapSum();
+ * obj->insert(key,val);
+ * int param_2 = obj->sum(prefix);
+ */

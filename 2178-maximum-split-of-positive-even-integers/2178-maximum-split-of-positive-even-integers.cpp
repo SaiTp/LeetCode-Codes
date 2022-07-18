753ae552aa56ac55667839f6c7f5f29dbbd218a7
SAiTEj <37739724+SaiTp@users.noreply.github.com>
2022-07-18 22:36:32 +0530

    Time: 293 ms (28.57%), Space: 40.5 MB (47.35%) - LeetHub

--- 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.cpp
+++ 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     vector<long long> maximumEvenSplit(long long finalSum) {
-        vector<long long int>res;
-        if(finalSum % 2)
-            return res;
-        long long int t=2,k=0,temp=2;
-        while(t<=finalSum)
+         if(finalSum%2!=0)return {};
+        vector<long long> ans;
+    
+        
+        for(long long i=2;finalSum-i>=i+2;i+=2)
         {
-            temp+=2;
-            t+=temp;
-            k++;
-        }
-        t=2;
-        temp=2;
-        while(k>1)
-        {
-            res.push_back(t);
-            finalSum-=t;
-            t+=2;
+            ans.push_back(i);
+            finalSum-=i;
             
-            k--;
-        }
-        res.push_back(finalSum);
-        // cout<<k;
-        return res;
+            
+        }        
+        
+        ans.push_back(finalSum);
+        
+        return ans;
     }
 };

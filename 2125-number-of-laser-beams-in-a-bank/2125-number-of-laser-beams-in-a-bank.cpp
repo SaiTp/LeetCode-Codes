bd822356dcbc2ed609be2e5d12c1e3c237407888
SAiTEj <37739724+SaiTp@users.noreply.github.com>
2022-07-18 19:46:49 +0530

    Time: 173 ms (47.22%), Space: 22.9 MB (98.40%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    int numberOfBeams(vector<string>& bank) {
-        vector<int>dev;
-        int ans=0;
-        for(int i=0;i<bank.size();i++)
-        {
-            int c=0;
-            for(int j=0;j<bank[i].size();j++)
-            {
-                if(bank[i][j]=='1')
-                    c++;
-            }
-            if(c)
-                dev.push_back(c);
+       int solve(string& s) {
+        int ans = 0;
+        for(char c : s) {
+            if(c == '1') ans++;
         }
-        // for(auto i:dev)
-        //     cout<<i;
-        if(dev.size()==0)
-            return 0;
-        for(int i=0;i<dev.size()-1;i++)
-            ans+=dev[i]*dev[i+1];
+        return ans;
+    }
+    
+    int numberOfBeams(vector<string>& bank) {
+        int ans = 0, prev = 0;
+        for(string& s : bank) {
+            int current = solve(s);
+            if(current != 0) {
+                ans += current * prev;
+                prev = current;            
+            }
+        }
         return ans;
     }
 };

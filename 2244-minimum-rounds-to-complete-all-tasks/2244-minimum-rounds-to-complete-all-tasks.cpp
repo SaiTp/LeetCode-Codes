c6570a7ef96f36ed44b06c4b252b1641ca60a37d
SAiTEj <37739724+SaiTp@users.noreply.github.com>
2022-07-20 11:11:37 +0530

    Time: 364 ms (41.37%), Space: 105.8 MB (6.68%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -1,37 +1,19 @@
 class Solution {
 public:
     int minimumRounds(vector<int>& tasks) {
-        unordered_map<int,int>mp;
-        int n=tasks.size();
-        if(n<2)
-            return -1;
-        for(auto i:tasks)
-            mp[i]++;
-        int ans=0;
-        for(auto i: mp)
+        unordered_map<int,long> mmap;
+        for(const auto& t : tasks)
+            ++mmap[t];
+        int total=0;
+        for (const auto& m : mmap)
         {
-            // cout<<i.first<<" "<<i.second<<endl;
-            if(i.second==1)
+            if ( m.second == 1)
                 return -1;
-            if(i.second%3==0)
-            {
-                ans+=i.second/3;
-            }
             else
             {
-                while(i.second>4)
-                {
-                    ans++;
-                    i.second-=3;
-                }
-                if(i.second==4)
-                    ans+=2;
-                else if (i.second==3 || i.second==2)
-                    ans++;
-                else
-                    return -1;
+                total +=(m.second+2)/3;
             }
         }
-        return ans;
+        return total;
     }
 };

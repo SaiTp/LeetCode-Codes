72e93a155c20b7190f20a2262865a0b6bbdcb67e
SAiTEj <37739724+SaiTp@users.noreply.github.com>
2022-07-25 11:45:20 +0530

    Time: 260 ms (97.87%), Space: 78.4 MB (88.43%) - LeetHub

--- 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.cpp
+++ 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
     vector<int> findingUsersActiveMinutes(vector<vector<int>>& logs, int k) {
-        unordered_map<int,unordered_set<int>>mp1;
-        unordered_map<int,int>mp2;
-        vector<int>res(k,0);
-        for(int i=0;i<logs.size();i++)
-        {
-            mp1[logs[i][0]].insert(logs[i][1]);
+     unordered_map<int, set<int>> m;
+        for (const auto& log : logs) {
+            const int id = log[0];
+            const int time = log[1];
+            m[id].insert(time);
         }
-        for(auto i:mp1)
-        {
-            // cout<<i.first<<" "<<i.second.size();
-            mp2[i.second.size()]++;
+        vector<int> ans(k);
+        for (const auto& [id, time_set] : m) {
+            ++ans[size(time_set) - 1];
         }
-        // cout<<endl;
-        for(auto i:mp2)
-        {
-            // cout<<i.first<<" "<<i.second;
-            res[i.first-1]=i.second;
-        }
-        return res;
+        return ans;
     }
 };

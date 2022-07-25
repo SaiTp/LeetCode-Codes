13bf00557bba7386fcbfef05de8ca69959975276
SAiTEj <37739724+SaiTp@users.noreply.github.com>
2022-07-25 15:34:46 +0530

    Time: 7 ms (63.81%), Space: 6.2 MB (61.43%) - LeetHub

--- 1860-incremental-memory-leak/1860-incremental-memory-leak.cpp
+++ 1860-incremental-memory-leak/1860-incremental-memory-leak.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     vector<int> memLeak(int m1, int m2) {
+       vector<int> res(3,0);
         int i=1;
-        while(i<=m1 || i<=m2)
-        {
-            if(m1>=m2)
-                m1-=i;
-            else
-                m2-=i;
+        while(i<=m1 || i<=m2){
+            if(m1>=m2) m1-=i;
+            else m2-=i;
             i++;
         }
-        return {i,m1,m2};
+        res[0]=i, res[1]=m1, res[2]=m2;
+        return res;
     }
 };

ed4cb1e44f21a34c8561a1d9532353d05d9fc79c
SAiTEj <37739724+SaiTp@users.noreply.github.com>
2022-07-25 12:04:35 +0530

    Time: 21 ms (70.96%), Space: 10.4 MB (92.47%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
     int numOfPairs(vector<string>& nums, string target) {
-        int ans=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            for(int j=0;j<nums.size();j++)
-            {
-                if(i==j)
-                    continue;
-                if(nums[i]+nums[j]==target)
-                    ans++;
+        int n = nums.size();
+        unordered_map<int, int> prefix_count;
+        for (int index=0; index<n; index++) {
+            if (nums[index].size() < target.size() &&
+                target.substr(0, nums[index].size()) == nums[index]) {
+                prefix_count[nums[index].size()]++;
             }
         }
-        return ans;
+        
+        int count = 0;
+        for (int index=0; index<n; index++) {
+            if (nums[index].size() < target.size() &&
+                target.substr(target.size() - nums[index].size()) == nums[index]) {
+                count += prefix_count[target.size() - nums[index].size()];
+                if (nums[index].size()*2 == target.size() &&
+                    target.substr(0, target.size()/2) == nums[index]) {
+                    count--;
+                }
+            }
+        }
+        return count;
     }
 };

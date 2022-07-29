22581fd4b249fc0a54090498c6818bb5c2c91986
SAiTEj <37739724+SaiTp@users.noreply.github.com>
2022-07-29 14:28:08 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (93.57%) - LeetHub

--- 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
+++ 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
     int maximumScore(int a, int b, int c) {
-        int ans=0;
-        vector<int>v{a,b,c};
-        while(1)
-        {
-            sort(v.begin(),v.end());
-            if(v[2]==0 && v[0]==0 || v[1]==0 &&v[2]==0 || v[0]==0 && v[1]==0)
-            break;
-            if(v[0])
-            v[0]--;
-            else
-                v[1]--;
-            v[2]--;
-            ans++;
+         int x = max(a,max(b,c));
+        int y = min(a,min(b,c));
+        int z = a+b+c-x-y;
+        // cout<<x<<y<<z;
+        if(z+y>x){
+            return (a+b+c)/2;
         }
-        return ans;
+        return min(x, y+z);
     }
 };

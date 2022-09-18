49900333c28e461851c28cd7d02aad77e95ca4d0
SAiTEj <37739724+SaiTp@users.noreply.github.com>
2022-09-18 10:13:39 +0530

    Time: 159 ms (28.57%), Space: 15.3 MB (14.29%) - LeetHub

--- 2414-length-of-the-longest-alphabetical-continuous-substring/2414-length-of-the-longest-alphabetical-continuous-substring.cpp
+++ 2414-length-of-the-longest-alphabetical-continuous-substring/2414-length-of-the-longest-alphabetical-continuous-substring.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     int longestContinuousSubstring(string s) {
-        int ans=1;
-        int i=0,j=1,n=s.size();
-        while(j<n)
-        {
-            while(j<n && s[j-1]+1==s[j])
-            {
-                j++;
-            }
-            ans=max(ans,j-i);
-            i=j;
-            j++;
+        int ans = 0; 
+        for (int i = 0, cnt = 0; i < s.size(); ++i) {
+            if (i == 0 || s[i-1]+1 != s[i]) cnt = 1; 
+            else ++cnt; 
+            ans = max(ans, cnt); 
         }
         return ans;
     }

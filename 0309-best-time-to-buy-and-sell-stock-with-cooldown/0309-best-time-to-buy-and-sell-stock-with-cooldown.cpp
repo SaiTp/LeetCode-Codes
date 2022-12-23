86130dd248dd7f53394c34a584f25579cf1910d4
SAiTEj <37739724+SaiTp@users.noreply.github.com>
2022-12-23 12:11:59 +0530

    Time: 12 ms (32.50%), Space: 11.3 MB (74.16%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+         int coolDown=0, sell=0, hold=std::numeric_limits<int>::min();
+        
+        for( int stockPrice_Day_i : prices){
+            
+            int prevCoolDown = coolDown, prevSell = sell, prevHold = hold;
+            
+             // Max profit of cooldown on Day i comes from either cool down of Day_i-1, or sell out of Day_i-1 and today Day_i is cooling day
+            coolDown = max(prevCoolDown, prevSell);
+            
+            // Max profit of sell on Day_i comes from hold of Day_i-1 and sell on Day_i
+            sell = prevHold + stockPrice_Day_i;
+            
+            // Max profit of hold on Day_i comes from either hold of Day_i-1, or cool down on Day_i-1 and buy on Day_i
+            hold = max(prevHold, prevCoolDown - stockPrice_Day_i);
+            
+        }
+        
+        // The state of max profit must be either sell or cool down
+        return max( sell, coolDown );
+    }
+};

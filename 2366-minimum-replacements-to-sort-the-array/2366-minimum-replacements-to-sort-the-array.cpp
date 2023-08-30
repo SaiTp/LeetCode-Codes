25b779601f7d72407fa301946b97eb28f36c16e8
SAiTEj <37739724+SaiTp@users.noreply.github.com>
2023-08-30 17:53:46 +0530

    Time: 219 ms (8.41%), Space: 54.6 MB (42.04%) - LeetHub

--- 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
@@ -4,6 +4,7 @@ public:
         long n = A.size(), x = 1e9, res = 0, k;
         for (int i = n - 1; i >= 0; --i) {
             k = (A[i] + x - 1) / x;
+            cout<<k<<endl;
             x = A[i] / k;
             res += k - 1;
         }

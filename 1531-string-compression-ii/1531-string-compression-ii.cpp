20666ba0bcdaa209d3e54834c05f4332c46b13ab
SAiTEj <37739724+SaiTp@users.noreply.github.com>
2023-12-28 17:40:07 +0530

    Time: 343 ms (61.80%), Space: 16.1 MB (69.66%) - LeetHub

--- 1531-string-compression-ii/1531-string-compression-ii.cpp
+++ 1531-string-compression-ii/1531-string-compression-ii.cpp
@@ -1,28 +1,33 @@
-int cache[101][27][101][101];
 class Solution {
 public:
-  int getLengthOfOptimalCompression(string s, int k) {    
-    memset(cache, -1, sizeof(cache));
-    // Min length of compressioned string of s[i:]    
-    // 1. last char is |last|
-    // 2. current run-length is len
-    // 3. we can delete k chars.
-    function<int(int, int, int, int)> dp = 
-      [&](int i, int last, int len, int k) {
-      if (k < 0) return INT_MAX / 2;
-      if (i >= s.length()) return 0;      
-      int& ans = cache[i][last][len][k];
-      if (ans != -1) return ans;
-      if (s[i] - 'a' == last) { 
-        // same as the previous char, no need to delete.
-        int carry = (len == 1 || len == 9 || len == 99);
-        ans = carry + dp(i + 1, last, len + 1, k);
-      } else {
-        ans = min(1 + dp(i + 1, s[i] - 'a', 1, k),  // keep s[i]
-                      dp(i + 1, last, len, k - 1)); // delete s[i]
-      }
-      return ans;
-    };
-    return dp(0, 26, 0, k);
-  }
+    int getLengthOfOptimalCompression(string s, int k) {
+        int n = s.length();
+        vector<vector<int>> dp(110, vector<int>(110, 9999)); // Initializing a 2D vector 'dp' of size 110x110 with value 9999
+        dp[0][0] = 0; // Initializing the base case where no characters and deletions exist
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= k; j++) {
+                int cnt = 0, del = 0;
+                for (int l = i; l >= 1; l--) {
+                    // Count the frequency of characters from 'i' to 'l'
+                    if (s[l - 1] == s[i - 1]) 
+                        cnt++;
+                    else 
+                        del++;
+                    
+                    // Check if the remaining allowed deletions are valid (j - del >= 0)
+                    if (j - del >= 0) {
+                        // Update the dp array based on the conditions
+                        dp[i][j] = min(dp[i][j],
+                            dp[l - 1][j - del] + 1 + (cnt >= 100 ? 3 : cnt >= 10 ? 2 : cnt >= 2 ? 1 : 0));
+                    }
+                }
+                
+                // If there are remaining allowed deletions (j > 0), consider the case without deleting current character
+                if (j > 0)
+                    dp[i][j] = min(dp[i][j], dp[i - 1][j - 1]);
+            }
+        }
+        return dp[n][k]; // Return the minimum length for 's' with at most 'k' deletions
+    }
 };

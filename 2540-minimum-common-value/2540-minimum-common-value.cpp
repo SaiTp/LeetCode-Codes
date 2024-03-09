5fa563049a8907493fc3081f8db609f33cb71a6c
SAiTEj <37739724+SaiTp@users.noreply.github.com>
2024-03-09 09:24:27 +0530

    Time: 60 ms (92.57%), Space: 52.9 MB (73.96%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int first = 0;
+        int second = 0;
+
+        // Traverse through both arrays with two pointers
+        // Increment the pointer with a smaller value at that index
+        // Return the first common element found
+        while (first < nums1.size() && second < nums2.size()) {
+            if (nums1[first] < nums2[second]) {
+                first++;
+            } else if (nums1[first] > nums2[second]) {
+                second++;
+            } else {
+                return nums1[first];
+            }
+        }
+
+        // Return -1 if there are no common elements
+        return -1;
+    }
+};

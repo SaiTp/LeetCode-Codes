26c7f7068fa8bb35f07342b9fc2a400dfcb674c9
SAiTEj <37739724+SaiTp@users.noreply.github.com>
2024-03-16 12:58:08 +0530

    Time: 81 ms (74.35%), Space: 87.7 MB (53.36%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findMaxLength(std::vector<int>& nums) {
+        std::unordered_map<int, int> sumMap;
+        int maxLength = 0;
+        int sum = 0;
+        
+        sumMap[0] = -1; // Initialize with 0 sum at index -1
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            // Increment sum for 1, decrement for 0
+            sum += (nums[i] == 1) ? 1 : -1;
+            
+            // If the same sum is encountered again, it means that
+            // the subarray between the previous occurrence and this
+            // one has equal number of 0s and 1s
+            if (sumMap.find(sum) != sumMap.end()) {
+                maxLength = std::max(maxLength, i - sumMap[sum]);
+            } else {
+                sumMap[sum] = i; // Store the index of first occurrence of sum
+            }
+        }
+        
+        return maxLength;
+    }
+};

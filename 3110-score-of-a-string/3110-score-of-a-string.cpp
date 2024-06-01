d21f2b4dd11393c7cfd449f2a62c3fd8f1b59f84
SAiTEj <37739724+SaiTp@users.noreply.github.com>
2024-06-01 09:00:57 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (42.17%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int sum=0,n=s.size();
+        for(int i=0;i<n-1;i++)
+        {
+            sum+=abs(s[i]-s[i+1]);
+        }
+        return sum;
+    }
+};
